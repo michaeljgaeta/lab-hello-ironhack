b43b130094d592e17295ebbc3d1fe4c91feae185
michaeljgaeta <michaeljgaeta@gmail.com>
2020-05-28 11:02:25 +0100

    split code

---
+++ src/components/InfoCard.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import logo from "../logo.svg";
+import "../App.css";
+
+const InfoCard = (props) => {
+  return (
+    <div className="icon-set">
+      <div className="icon-individual">
+        <img src="images/icon1.png" alt="icon" />
+        <h2>Declarative</h2>
+        <p>React makes it painless to create interactive UIs.</p>
+      </div>
+      <div className="icon-individual">
+        <img src="images/icon2.png" alt="icon" />
+        <h2>Components</h2>
+        <p>Build encapsulated components that manage their state.</p>
+      </div>
+      <div className="icon-individual">
+        <img src="images/icon3.png" alt="icon" />
+        <h2>Single-Way</h2>
+        <p>A set of immutable values are passed to the component's.</p>
+      </div>
+      <div className="icon-individual">
+        <img src="images/icon4.png" alt="icon" />
+        <h2>JSX</h2>
+        <p>Statically-typed, designed to run on modern browsers.</p>
+      </div>
+    </div>
+  );
+};
+
+export default InfoCard;
